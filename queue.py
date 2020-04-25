4f516e409c402c01819a1d4ec396ba797db3efdb
GSCrawley <gscrawley@gmail.com>
2020-04-24 20:02:08 -0700

    now LinkedQueue also passing

--- queue.py
+++ queue.py
@@ -1,4 +1,5 @@
 #!python
+
 from linkedlist import LinkedList
 
 
@@ -20,35 +21,44 @@ class LinkedQueue(object):
 
     def is_empty(self):
         """Return True if this queue is empty, or False otherwise."""
-        if self.length() == 0:
+        # TODO: Check if empty
+        if self.list.head == None:
             return True
         else:
             return False
 
     def length(self):
         """Return the number of items in this queue."""
-        return self.list.length
+        # TODO: Count number of items
+        return self.list.length()
 
     def enqueue(self, item):
         """Insert the given item at the back of this queue.
         Running time: O(???) – Why? [TODO]"""
-        return self.list.append(item)
-            
+        # TODO: Insert given item
+        self.list.append(item)
 
     def front(self):
         """Return the item at the front of this queue without removing it,
         or None if this queue is empty."""
-        return None if self.is_empty() else self.list.head.data
+        # TODO: Return front item, if any
+        if self.list.is_empty() == True:
+            return None
+        else:
+            return self.list.head.data
 
     def dequeue(self):
         """Remove and return the item at the front of this queue,
         or raise ValueError if this queue is empty.
         Running time: O(???) – Why? [TODO]"""
-        if self.is_empty():
-            raise ValueError('This queue is empty')
-        beginning = self.list.head.data
-        self.list.delete(beginning)
-        return beginning
+        # TODO: Remove and return front item, if any
+        # truth = self.list.is_empty()
+        if self.list.is_empty() == True:
+            raise ValueError
+        else:
+            dequeued = self.list.head.data
+            self.list.delete(self.list.head.data)
+            return dequeued
 
 
 # Implement ArrayQueue below, then change the assignment at the bottom
@@ -69,47 +79,54 @@ class ArrayQueue(object):
 
     def is_empty(self):
         """Return True if this queue is empty, or False otherwise."""
+        # TODO: Check if empty
         if len(self.list) == 0:
             return True
         else:
             return False
 
-
     def length(self):
         """Return the number of items in this queue."""
-        count = 0
-        for _ in range(len(self.list)):
-            count += 1
-        return count
+        # TODO: Count number of items
+        if len(self.list) == 0:
+            return 0
+        else:
+            return len(self.list)
+
 
     def enqueue(self, item):
         """Insert the given item at the back of this queue.
         Running time: O(???) – Why? [TODO]"""
+        # TODO: Insert given item
         self.list.append(item)
 
     def front(self):
         """Return the item at the front of this queue without removing it,
         or None if this queue is empty."""
         # TODO: Return front item, if any
-        if self.is_empty():
-            return None
-        else:
+        if len(self.list) > 0:
             return self.list[0]
+        else:
+            return None
 
     def dequeue(self):
         """Remove and return the item at the front of this queue,
         or raise ValueError if this queue is empty.
         Running time: O(???) – Why? [TODO]"""
-        # TODO: Remove and return front item, if any(pay attention to this)
-        if self.is_empty():
-            raise ValueError('This queue is empty')
+        # TODO: Remove and return front item, if any
+        if len(self.list) > 0:
+            dequeued = self.list[0]
+
+            self.list.remove(self.list[0])
+
         else:
-            front = self.front()
-            self.list.pop(0)
-            return front
+            raise ValueError
+
+        return dequeued
+
 
 
 # Implement LinkedQueue and ArrayQueue above, then change the assignment below
 # to use each of your Queue implementations to verify they each pass all tests
-# Queue = LinkedQueue
-Queue = ArrayQueue
+Queue = LinkedQueue
+# Queue = ArrayQueue
--- stack.py
+++ stack.py
@@ -1,15 +1,13 @@
-#!python
-
 from linkedlist import LinkedList
+
 
 # Implement LinkedStack below, then change the assignment at the bottom
 # to use this Stack implementation to verify it passes all tests
-
 class LinkedStack(object):
 
     def __init__(self, iterable=None):
-#         """Initialize this stack and push the given items, if any."""
-#         # Initialize a new linked list to store the items
+        """Initialize this stack and push the given items, if any."""
+        # Initialize a new linked list to store the items
         self.list = LinkedList()
         if iterable is not None:
             for item in iterable:
@@ -17,46 +15,57 @@ class LinkedStack(object):
 
     def __repr__(self):
         """Return a string representation of this stack."""
-        return 'stack({} items, top={})'.format(self.length(), self.peek())
+        return 'Stack({} items, top={})'.format(self.length(), self.peek())
 
     def is_empty(self):
         """Return True if this stack is empty, or False otherwise."""
         # TODO: Check if empty
-        return self.list.is_empty()
-
+        if self.list.head == None:
+            return True
+        else:
+            return False
 
     def length(self):
-#         """Return the number of items in this stack."""
-#         # TODO: Count number of items
-        return self.list.length
-
+        """Return the number of items in this stack."""
+        # TODO: Count number of items
+        node_count = 0
+        if self.list.size == 0:
+            return node_count
+        else:
+            print("Size: ",self.list.size)
+            return self.list.size
 
     def push(self, item):
-#         """Insert the given item on the top of this stack.
-#         Running time: O(???) – Why? [TODO]"""
-#         # TODO: Push given item
-        return self.list.prepend(item)
+        """Insert the given item on the top of this stack.
+        Running time: O(???) – Why? [TODO]"""
+        # TODO: Push given item
+        if self.list.head == None:
+            self.list.append(item)
+        else:
+            self.list.append(item)
 
     def peek(self):
-#         """Return the item on the top of this stack without removing it,
-#         or None if this stack is empty."""
-#         # TODO: Return top item, if any
-        if self.is_empty():
-            return None
-        else: 
-            return self.list.head.data
-    
-    def pop(self):
-#         """Remove and return the item on the top of this stack,
-#         or raise ValueError if this stack is empty.
-#         Running time: O(???) – Why? [TODO]"""
-#         # TODO: Remove and return top item, if any
-        if self.is_empty():
-            raise ValueError("Cannot pop from an empty stack")
-        item = self.list.head.data            
-        self.list.delete(item)
-        return item
+        """Return the item on the top of this stack without removing it,
+        or None if this stack is empty."""
+        # TODO: Return top item, if any
+        if self.is_empty() == True:
+            return
+        else:
+            return self.list.tail.data
 
+    def pop(self):
+        """Remove and return the item on the top of this stack,
+        or raise ValueError if this stack is empty.
+        Running time: O(???) – Why? [TODO]"""
+        # TODO: Remove and return top item, if any
+
+        if self.is_empty() == True:
+            print("test")
+            raise ValueError
+        else:
+            popped = self.list.tail.data
+            self.list.delete(self.list.tail.data)
+            return popped
 
 # Implement ArrayStack below, then change the assignment at the bottom
 # to use this Stack implementation to verify it passes all tests
@@ -77,45 +86,54 @@ class ArrayStack(object):
     def is_empty(self):
         """Return True if this stack is empty, or False otherwise."""
         # TODO: Check if empty
-        if self.length() == 0:
+        if len(self.list) == 0:
             return True
-        else: 
+        else:
             return False
 
     def length(self):
         """Return the number of items in this stack."""
         # TODO: Count number of items
+
         return len(self.list)
 
     def push(self, item):
         """Insert the given item on the top of this stack.
         Running time: O(???) – Why? [TODO]"""
-        self.list.append(item)
+        # TODO: Insert given item
+        return self.list.append(item)
 
     def peek(self):
         """Return the item on the top of this stack without removing it,
         or None if this stack is empty."""
         # TODO: Return top item, if any
-        if self.is_empty():
+        if len(self.list) == 0:
             return None
-        else:
-            return self.list[-1]
+
+        return self.list[len(self.list) -1]
 
     def pop(self):
         """Remove and return the item on the top of this stack,
         or raise ValueError if this stack is empty.
         Running time: O(???) – Why? [TODO]"""
         # TODO: Remove and return top item, if any
-        if self.is_empty():
-            raise ValueError('This stack is empty')
+        if self.is_empty() == True:
+            raise ValueError
+        if len(self.list) == 0:
+            return
         else:
-            return self.peek() and self.list.pop(-1)
-    
+            print(len(self.list)-1)
+            popped = self.list[-1]
+            self.list.pop(len(self.list)-1)
+        return popped
+
+
 # Implement LinkedStack and ArrayStack above, then change the assignment below
 # to use each of your Stack implementations to verify they each pass all tests
-# stack = LinkedStack()
-stack = ArrayStack()
-stack.push(1)
-stack.push(4)
-stack.push(5)
-print(stack)
+Stack = LinkedStack
+
+# Stack = ArrayStack
+# Stack.push(1)
+# Stack.push(4)
+# Stack.push(9)
+# print(Stack)
